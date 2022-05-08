808be2f1014d648aa12bb2635cf885e2a72f8638
pauloemil <prog.paulo@hotmail.com>
2022-05-08 10:06:09 +0200

    use env vars

--- server/connection.js
+++ server/connection.js
@@ -1,10 +1,11 @@
-const Pool = require('pg').Pool
+require("dotenv").config();
+const Pool = require("pg").Pool;
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'grad',
-    password: 'peter.data',
-    port: 5432,
-})
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.DB_PORT,
+});
 
-module.exports = pool
+module.exports = pool;
