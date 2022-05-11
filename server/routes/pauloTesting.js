ca0cc6c6d0504605d68d32282986091a0b9fc9b4
pauloemil <prog.paulo@hotmail.com>
2022-05-11 13:38:37 +0200

    store the voice, convert from m4a to wav then get the text out of it

---
+++ server/routes/pauloTesting.js
@@ -0,0 +1,71 @@
+// for Testing purposes
+const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
+const ffmpeg = require("fluent-ffmpeg");
+ffmpeg.setFfmpegPath(ffmpegPath);
+const multer = require("multer");
+const path = require("path");
+const fromFile = require("../configs/SpeachRecognition");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + "-" + Date.now() + ".m4a");
+  },
+});
+
+//upload parameters for multer
+const upload = multer({
+  storage: storage,
+  limits: { filesize: 1024 * 1024 * 5 },
+});
+
+function convertFileFormat(file, destination, error, progressing, finish) {
+  ffmpeg(file)
+    .on("error", (err) => {
+      console.log("An error occurred: " + err.message);
+      if (error) {
+        error(err.message);
+      }
+    })
+    .on("progress", (progress) => {
+      // console.log(JSON.stringify(progress));
+      console.log("Processing: " + progress.targetSize + " KB converted");
+      if (progressing) {
+        progressing(progress.targetSize);
+      }
+    })
+    .on("end", () => {
+      console.log("converting format finished !");
+      if (finish) {
+        finish();
+      }
+    })
+    .save(destination);
+}
+
+const Router = require("express").Router();
+Router.post("/upload-voice", upload.single("voice"), (req, res) => {
+  //   console.log("body", req.file);
+  convertFileFormat(
+    "G:\\Development\\gradproj_backend\\graduation_project\\server\\uploads\\" +
+      req.file.filename,
+    "G:\\Development\\gradproj_backend\\graduation_project\\server\\uploads\\" +
+      req.file.filename +
+      ".wav",
+    function (errorMessage) {},
+    null,
+    async function () {
+      const obj = fromFile(
+        "G:\\Development\\gradproj_backend\\graduation_project\\server\\uploads\\" +
+          req.file.filename +
+          ".wav"
+      ); // make it syncabla
+      // delete the records after use
+    }
+  );
+
+  res.json({ text: "this is your voice record you stupid shit bastered!" });
+});
+
+module.exports = Router;
