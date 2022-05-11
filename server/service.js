cd563e245a92b5bd6ce5ea4a91bd8abf9bf51156
pauloemil <prog.paulo@hotmail.com>
2022-05-11 13:37:59 +0200

    add paulo route

--- server/service.js
+++ server/service.js
@@ -1,13 +1,20 @@
-const express = require('express')
-const chats = require('./routes/chats')
-const register = require('./routes/register')
-const login = require('./routes/login')
-require('dotenv').config()
+const express = require("express");
+const chats = require("./routes/chats");
+const register = require("./routes/register");
+const login = require("./routes/login");
+const paulo = require("./routes/pauloTesting"); // for Testing purposes
+require("dotenv").config();
+const app = express();
 
-const app = express()
-app.use('/chats', chats)
-app.use('/register', register)
-app.use('/login', login)
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.listen(process.env.PORT, console.log(`listening on port ${process.env.PORT}...`))
+app.use("/chats", chats);
+app.use("/register", register);
+app.use("/login", login);
+app.use("/paulo", paulo); // for Testing purposes
+
+app.listen(
+  process.env.PORT,
+  console.log(`listening on port ${process.env.PORT}...`)
+);
