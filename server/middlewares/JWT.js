eb693cd874dcb0d52567e7be135abc4128d8c9a7
unknown <petermourad.work@gmail.com>
2022-05-12 13:12:46 +0200

    added refresh token

--- server/middlewares/JWT.js
+++ server/middlewares/JWT.js
@@ -1,18 +1,17 @@
-const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken")
 
 function GenerateToken(req) {
-    const token = jwt.sign({ id: req.id, password: req.password },
-        process.env.JWT_SECRET_KEY, { expiresIn: process.env.JWT_EXPIRES_IN })
-    return token
-}
-
-function VerifyToken(token) {
-    return jwt.verify(token, process.env.JWT_SECRET_KEY, function (err, decoded) {
-        if (err) return err.message
-        if ((decoded.exp * 1000 - Date.now()) / 1000 < 0) return false
-        return true
-    })
+    const access_token = jwt.sign(
+        { id: req.id },
+        process.env.JWT_SECRET_KEY,
+        { expiresIn: process.env.ACCESS_TOKEN_EXPIRE_TIME }
+    )
+    const refresh_token = jwt.sign(
+        {id: req.id}, 
+        process.env.JWT_SECRET_KEY, 
+        { expiresIn: process.env.REFRESH_TOKEN_EXPIRE_TIME }
+    )
+    return { access_token: access_token, refresh_token: refresh_token }
 }
 
 module.exports.GenerateToken = GenerateToken
-module.exports.VerifyToken = VerifyToken
