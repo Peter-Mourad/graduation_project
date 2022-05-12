6bb743038c1a205bd7d7755099e054d874910422
unknown <petermourad.work@gmail.com>
2022-05-12 13:18:46 +0200

    added auth routes

--- server/service.js
+++ server/service.js
@@ -1,11 +1,15 @@
 const express = require('express')
+
+const auth = require('./middlewares/auth')
+const jwt = require('./middlewares/JWT')
+
 const chats = require('./routes/chats')
 const register = require('./routes/register')
 const login = require('./routes/login')
-const auth = require('./middlewares/auth')
-const jwt = require('./middlewares/JWT')
 const refresh_token = require('./routes/regenerate_token')
 const reset_password = require('./routes/reset_passwrod')
+// const paulo_testing = require('./routes/pauloTesting')
+
 require('dotenv').config()
 
 const app = express()
@@ -14,6 +18,8 @@ app.use('/register', register)
 app.use('/login', login)
 app.use('/refresh', refresh_token)
 app.use('/reset_password', reset_password)
-app.use(express.json())
+// app.use('/paulo_testing', paulo_testing)
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.listen(process.env.PORT, console.log(`listening on port ${process.env.PORT}...`))
+app.listen( process.env.PORT,console.log(`listening on port ${process.env.PORT}...`))
