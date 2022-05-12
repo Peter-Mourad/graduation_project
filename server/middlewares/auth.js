457cada8e9f978c1b9b8f08662f68e54a7e97793
unknown <petermourad.work@gmail.com>
2022-05-12 13:25:21 +0200

    initial commit

---
+++ server/middlewares/auth.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+const VerifyToken = function (req, res, next) {
+    const access_token = req.headers["access_token"]
+    if (!access_token) {
+        return res.status(403).send({ error: 'access token is required!' })
+    }
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
+        if(err)return res.send({error: err.message})
+        if ((decoded.exp * 1000 - Date.now()) / 1000 < 0) return res.status(401).send({ error: 'Invalid token' })  
+    })
+
+    return next()
+}
+
+module.exports = VerifyToken
