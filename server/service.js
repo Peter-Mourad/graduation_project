15e5286458019caf5e8811b0352fbd5bfe85d56e
unknown <petermourad.work@gmail.com>
2022-05-16 19:17:11 +0200

    added route reset-password

--- server/service.js
+++ server/service.js
@@ -8,18 +8,20 @@ const register = require('./routes/register')
 const login = require('./routes/login')
 const refresh_token = require('./routes/regenerate_token')
 const reset_password = require('./routes/reset_passwrod')
-const paulo_testing = require('./routes/pauloTesting')
+const paulo_testing = require("./routes/pauloTesting")
 
 require('dotenv').config()
 
 const app = express()
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use('/chats', chats)
 app.use('/register', register)
 app.use('/login', login)
 app.use('/refresh', refresh_token)
-app.use('/reset_password', reset_password)
-app.use('/paulo_testing', paulo_testing)
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use('/reset-password', reset_password)
+app.use("/paulo", paulo_testing)
 
 app.listen( process.env.PORT,console.log(`listening on port ${process.env.PORT}...`))
