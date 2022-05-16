d5e87b9708d254ca27ce0189047b55487257922a
unknown <petermourad.work@gmail.com>
2022-05-16 19:45:31 +0200

    modified status codes

--- server/routes/regenerate_token.js
+++ server/routes/regenerate_token.js
@@ -1,8 +1,8 @@
-const jwt = require("jsonwebtoken")
-const express = require('express')
-const router = express.Router()
+const jwt = require("jsonwebtoken");
+const express = require("express");
+const router = express.Router();
 
-router.use(express.json())
+router.use(express.json());
 
 router.post('/', (req, res) => {
     const refresh_token = req.body.refresh_token
@@ -22,4 +22,18 @@ router.post('/', (req, res) => {
     })
 })
 
-module.exports = router
+    if ((decoded.exp * 1000 - Date.now()) / 1000 < 0) {
+      res.status(401);
+      return res.send({ error: "Expired token" });
+    }
+
+    const access_token = jwt.sign(
+      { id: decoded.id },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: process.env.ACCESS_TOKEN_EXPIRE_TIME }
+    );
+    return res.send({ access_token: access_token });
+  });
+});
+
+module.exports = router;
