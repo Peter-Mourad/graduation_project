ccafba94cd141cefed9aa40277e6945b27921be8
unknown <petermourad.work@gmail.com>
2022-05-18 14:42:30 +0200

    updated responses in loading chats route

--- server/routes/home.js
+++ server/routes/home.js
@@ -1,22 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../connection');
+const auth = require('../middlewares/auth')
 
 router.use(express.json());
 
 // create chat
-router.get('/:id', async (req, res) => {
-    const result = await pool.query(`SELECT c.chat_name, c.chat_id , m.message 
-		FROM public.chat c 
-         	INNER JOIN public.users u 
-			ON u.id = c.user_id
-       	INNER JOIN (SELECT DISTINCT ON (m.chat_id) m.chat_id , m.message 
-       				FROM public.message m 
-	  				ORDER BY m.chat_id ,m.delivered_time DESC) m 
-			ON c.chat_id = m.chat_id 
-       	WHERE u.id = '${req.params.id}'
-       	ORDER BY c.chat_id DESC `);
-    res.send(result.rows);
+router.get('/:id', auth, (req, res) => {
+    pool.query(`SELECT c.chat_name, c.chat_id , m.message 
+				FROM public.chat c 
+				INNER JOIN public.users u 
+					ON u.id = c.user_id
+				INNER JOIN (SELECT DISTINCT ON (m.chat_id) m.chat_id , m.message 
+							FROM public.message m 
+							ORDER BY m.chat_id ,m.delivered_time DESC) m 
+					ON c.chat_id = m.chat_id 
+				WHERE u.id = '${req.params.id}'
+				ORDER BY c.chat_id DESC `,
+		(err, result) => {
+			if (err) return res.status(400).send({ error: err.message });
+			return res.send(result.rows);
+		}
+	);
 });
 
 module.exports = router;
