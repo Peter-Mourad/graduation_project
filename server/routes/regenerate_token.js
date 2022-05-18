05739d4453b76b6308b19a3232e8d14f20deb5cb
unknown <petermourad.work@gmail.com>
2022-05-18 18:59:10 +0200

    removed unused block of code

--- server/routes/regenerate_token.js
+++ server/routes/regenerate_token.js
@@ -5,25 +5,19 @@ const router = express.Router();
 router.use(express.json());
 
 router.post("/", (req, res) => {
-  const refresh_token = req.body.refresh_token;
-  jwt.verify(refresh_token, process.env.JWT_SECRET_KEY, (err, decoded) => {
-    if (err) {
-      res.status(401);
-      return res.send({ error: "refresh token isn't valid" });
-    }
+    const refresh_token = req.body.refresh_token;
+    jwt.verify(refresh_token, process.env.JWT_SECRET_KEY, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ error: "refresh token isn't valid" });
+        }
 
-    if ((decoded.exp * 1000 - Date.now()) / 1000 < 0) {
-      res.status(401);
-      return res.send({ error: "Expired token" });
-    }
-
-    const access_token = jwt.sign(
-      { id: decoded.id },
-      process.env.JWT_SECRET_KEY,
-      { expiresIn: process.env.ACCESS_TOKEN_EXPIRE_TIME }
-    );
-    return res.send({ access_token: access_token });
-  });
+        const access_token = jwt.sign(
+          { id: decoded.id },
+          process.env.JWT_SECRET_KEY,
+          { expiresIn: process.env.ACCESS_TOKEN_EXPIRE_TIME }
+        );
+        return res.send({ access_token: access_token });
+    });
 });
 
 module.exports = router;
