575c66319e02764dad0759b5f9af29b56c2cd942
pauloemil <prog.paulo@hotmail.com>
2022-05-19 16:16:25 +0200

    Edit response status codes

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,16 +1,16 @@
 const jwt = require("jsonwebtoken");
 
 const VerifyToken = function (req, res, next) {
-    const authHeader = req.headers.authorization;
-    const access_token = authHeader.split(" ")[1];
-    if (!access_token) {
-        return res.status(403).send({ error: 'access token is required!' })
-    }
-    jwt.verify(access_token, process.env.JWT_SECRET_KEY, (err, decoded) => {
-        if (err) return res.send({ error: 'Invalid token' });
-        req.user = decoded;
-        next();
-    });
+  const authHeader = req.headers.authorization;
+  const access_token = authHeader.split(" ")[1];
+  if (!access_token) {
+    return res.status(403).send({ error: "access token is required!" });
+  }
+  jwt.verify(access_token, process.env.JWT_SECRET_KEY, (err, decoded) => {
+    if (err) return res.status(401).send({ error: "Invalid token" });
+    req.user = decoded;
+    next();
+  });
 };
 
-module.exports = VerifyToken
+module.exports = VerifyToken;
