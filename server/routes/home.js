ccd6f9683c138526eeafdfe57c97e058e2f8b5a1
unknown <petermourad.work@gmail.com>
2022-05-19 16:13:29 +0200

    added time parsing in loading last message

--- server/routes/home.js
+++ server/routes/home.js
@@ -21,26 +21,38 @@ router.get('/', auth, (req, res) => {
 				ORDER BY c.chat_id DESC `,
 		(err, result) => {
 			if (err) return res.status(400).send({ error: err.message });
+			for (var i = 0; i < result.rowCount; i++){
+				if (result.rows[i].message!=null) {
+					result.rows[i].delivered_time = time_parse(result.rows[i].delivered_time);
+				}
+			}
 			return res.send(result.rows);
 		}
 	);
 });
 
 const time_parse = (cur) => {
-	const date = new Date(cur);
-	console.log(date, date.getDate(), date.getMonth(), date.getFullYear());
-	// var year = parseInt(cur.substring(0, 4));
-	// var month = parseInt(cur.substring(5, 7));
-	// var day = parseInt(cur.substring(8, 10));
-	// var hour = parseInt(cur.substring(11, 13));
-	// var min = parseInt(cur.substring(14, 16));
-	// var sec = parseInt(cur.substring(17, cur.length - 1));
-	const today = new Date();
-	// console.log(today.getDate(),day);
-	// if (today.getDate() == day) {
-	// 	return day;
-	// }
-	return today;
+	const temp_date = new Date(cur),temp_cur_date = new Date(Date.now());
+	let date = temp_date.toString(),cur_date = temp_cur_date.toString();
+	const new_date = date.split(/[ :]+/), new_cur_date = cur_date.split(/[ :]+/);
+	
+	var hour = parseInt(new_date[4]);
+	var time = "AM";
+	if (hour >= 12) {
+		if(hour> 12)
+			hour -= 12;
+		time = "PM";
+	}
+	var day = parseInt(new_date[2]), cur_day = parseInt(new_cur_date[2]);
+	if (day == cur_day) {
+		return `${hour}:${new_date[5]} ${time}`;
+	}
+	else if (day == cur_day - 1) {
+		return `yesterday`;
+	}
+	else {
+		return `${new_date[2]}/${new_date[1]}/${new_date[3]}`;
+	}
 }
 
 module.exports = router;
